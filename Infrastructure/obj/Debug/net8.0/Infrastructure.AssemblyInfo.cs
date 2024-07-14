84d014a9d79fce2354b31c2fc94a5b99fa5806da
hasan <hassun.h98@gmail.com>
2024-07-14 16:33:37 +0300

    NEW UPDATE

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c123cd4a6963a5adec3058a27e821453efb674fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d46c779ed3ccd53352c9bdb3b162c95291b8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
