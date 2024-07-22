90df9301d25e4ac2480498d74c5ca83dabf94486
hasan <hassun.h98@gmail.com>
2024-07-22 11:30:00 +0300

    -NEW UPDATE

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f219f50191a25b73eb942896be760b8c9dc2aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5289df0e6f56b730742e060910a96ceb39687d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { DeliveryMethod } from '../shared/models/DeliveryMethod';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+baseUrl = environment.apiUrl;
 
+constructor(private http:HttpClient){}
+
+getDeliveryMethod(){
+  return this.http.get<DeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+    map(dm => {
+      return dm.sort((a,b)=>b.price - a.price)
+    })
+  )
+}
  
 }
